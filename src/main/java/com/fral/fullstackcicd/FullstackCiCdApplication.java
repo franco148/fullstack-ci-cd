21703bd5271363e418c956d698b5d06cd6cd387d
Franco Arratia <franck_ral@hotmail.com>
2021-03-20 21:51:23 -0400

    fullstack-ci-cd: This is the initial structure of the repository for a complete CI/CD POC

---
+++ src/main/java/com/fral/fullstackcicd/FullstackCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.fral.fullstackcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fral/fullstackcicd/FullstackCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fral.fullstackcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
