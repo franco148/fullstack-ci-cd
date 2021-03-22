c2dd170d5588fb7f75508f816fffb23057e69655
Franco Arratia <franck_ral@hotmail.com>
2021-03-21 20:57:25 -0400

    fullstack-ci-cd: Implementing the frontend of the application, integrated with antd

---
+++ src/frontend/src/client.js
@@ -0,0 +1,18 @@
+
+import fetch from 'unfetch';
+
+const checkStatus = response => {
+    if (response.ok) {
+        return response;
+    }
+    // convert non-2xx HTTP responses into errors:
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+}
+
+export const getAllStudents = () =>
+    fetch("api/v1/students")
+        .then(checkStatus);
+
+
