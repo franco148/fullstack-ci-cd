95aeb624ccb34385afb55b5b1205d137c647cbca
franco.fral <franck_ral@hotmail.com>
2021-04-01 17:26:46 -0400

    fullstack-ci-cd: Fixed issues in the domain model

--- src/main/java/com/fral/fullstackcicd/domain/Student.java
+++ src/main/java/com/fral/fullstackcicd/domain/Student.java
@@ -3,17 +3,32 @@ package com.fral.fullstackcicd.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @ToString
 @Getter
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Entity
+@Table(name = "students")
 public class Student {
 
+    @Id
+    @SequenceGenerator(
+        name = "students_sequence",
+        sequenceName = "students_sequence",
+        initialValue = 2,
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        generator = "students_sequence",
+        strategy = GenerationType.SEQUENCE
+    )
     private Long id;
     private String name;
     private String email;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 }
