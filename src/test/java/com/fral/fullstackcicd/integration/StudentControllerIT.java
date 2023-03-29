eff5c96eede37b605b1981243c12eeddd532927d
Franco Arratia <franz.arratia@avantica.net>
2023-03-29 16:14:11 -0400

    fullstack-ci-cd-unittests: Implemented the integration tests

--- src/test/java/com/fral/fullstackcicd/integration/StudentControllerIT.java
+++ src/test/java/com/fral/fullstackcicd/integration/StudentControllerIT.java
@@ -1,5 +1,7 @@
 package com.fral.fullstackcicd.integration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fral.fullstackcicd.domain.Gender;
 import com.fral.fullstackcicd.domain.Student;
@@ -12,12 +14,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -55,12 +61,57 @@ public class StudentControllerIT {
 
         List<Student> students = studentRepository.findAll();
 //        assertThat(students)
-//                .usingRecursiveComparison()
-//                .ignoringFields("id")
-//                .asList()
+//                .usingElementComparatorIgnoringFields("id")
 //                .contains(student);
+
         then(students)
                 .usingElementComparatorIgnoringFields("id")
                 .contains(student);
+    }
+
+    @Test
+    void canDeleteStudent() throws Exception {
+        // given
+        String name = faker.name().name();
+        String email = faker.internet().emailAddress();
+
+        Student student = new Student();
+        student.setName(name);
+        student.setEmail(email);
+        student.setGender(Gender.MALE);
+
+        mockMvc.perform(post("/api/v1/students")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(student)))
+                .andExpect(status().isOk());
+
+        MvcResult retrievedStudentsResult = mockMvc.perform(get("/api/v1/students")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String resultAsString = retrievedStudentsResult
+                .getResponse().getContentAsString();
+
+        List<Student> retrievedStudents = objectMapper.readValue(
+                resultAsString,
+                new TypeReference<List<Student>>() {
+                }
+        );
+
+        Long studentId = retrievedStudents.stream()
+                .filter(s -> s.getEmail().equals(email))
+                .map(Student::getId)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student with email: " + email + " not found"));
+
+        // when
+        ResultActions deleteResult = mockMvc.perform(delete("/api/v1/students/" + studentId));
+
+        // then
+        deleteResult.andExpect(status().isOk());
+
+        boolean exists = studentRepository.existsById(studentId);
+        then(exists).isFalse();
     }
 }
