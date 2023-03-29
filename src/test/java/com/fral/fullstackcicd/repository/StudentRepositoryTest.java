3d2b38989ec5eca6e526822d3dc1e3bdd509a522
Franco Arratia <franz.arratia@avantica.net>
2023-03-29 10:04:07 -0400

    fullstack-ci-cd-unittests: Unit testing the DAL layer

---
+++ src/test/java/com/fral/fullstackcicd/repository/StudentRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.fral.fullstackcicd.repository;
+
+import com.fral.fullstackcicd.domain.Gender;
+import com.fral.fullstackcicd.domain.Student;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository underTest;
+
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void itShouldCheckWhenStudentEmailExists() {
+        // given
+        String email = "jamila@gmail.com";
+        Student student = new Student();
+        student.setName("Jamila");
+        student.setEmail(email);
+        student.setGender(Gender.FEMALE);
+
+        underTest.save(student);
+
+        // when
+        boolean expected = underTest.selectExistsEmail(email);
+
+        // then
+        assertThat(expected).isTrue();
+    }
+
+    @Test
+    void itShouldCheckWhenStudentEmailDoesNotExists() {
+        // given
+        String email = "jamila@gmail.com";
+
+        // when
+        boolean expected = underTest.selectExistsEmail(email);
+
+        // then
+        assertThat(expected).isFalse();
+    }
+}
